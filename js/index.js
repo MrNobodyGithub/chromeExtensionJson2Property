57c6178a360327dbd08b2e347d0fa02edbc0abf1
nemo_x <1508122971@qq.com>
2020-08-27 17:07:35 +0800

    添加  对swift 中 SwiftyJSON、ObjectMapper、HandyJSON 的支持

--- js/index.js
+++ js/index.js
@@ -1,31 +1,164 @@
 
 
-$('#btnid1').click(e => {
+ 
+$('#btnHandyJSON').click(e => {
 
-	var data= { "Type": "Coding", "Height":100 };
+	var data= { "type": "Coding", "height":100 };
     var v1 = document.getElementById("textarea_in").value;
 	if (v1 != "") {
         v1 = strToJson(v1);
-        alert(v1);
+        alert("SwiftyJSON success");
 	}   else{
 		v1 =  data;
 	} 
 
     var json = v1;//document.getElementById("textarea_in").value;
     var listStr = "";
+    var charType = "String"
     for( var key in json){
     	var value = json[key];
     	// print(key +  "  " + value);
     	var check = typeof(value);
     	if (check == "string") {
-
+    		charType = "String"
     	}else if(check == "number"){
+    		charType = "Int"
 
+    	} 
+    	listStr = listStr + "var " + key + ":" + charType+"?" + "\n" 
+    }
+    listStr = listStr + "required init(){}\n"
+ 
+
+    document.getElementById("textarea_out").value = listStr;
+    copyToClipboard(listStr);
+
+});
+
+
+$('#btnObjectMapper').click(e => {
+
+	var data= { "type": "Coding", "height":100 };
+    var v1 = document.getElementById("textarea_in").value;
+	if (v1 != "") {
+        v1 = strToJson(v1);
+        alert("SwiftyJSON success");
+	}   else{
+		v1 =  data;
+	} 
+
+    var json = v1;//document.getElementById("textarea_in").value;
+    var listStr = "";
+    var charType = "String"
+    for( var key in json){
+    	var value = json[key];
+    	// print(key +  "  " + value);
+    	var check = typeof(value);
+    	if (check == "string") {
+    		charType = "String"
+    	}else if(check == "number"){
+    		charType = "Int"
+
+    	} 
+    	listStr = listStr + "var " + key + ":" + charType+"?" + "\n" 
+    }
+    listStr = listStr + "\n"
+
+    listStr = listStr + "init?(map:Map){" + "\n"
+    listStr = listStr + "}\n"
+
+    listStr = listStr + "mutating func mappint(map:Map){ \n"
+    for( var key in json){
+    	var value = json[key]; 
+    	listStr = listStr + "    "+key + "  <- map[\""+ key + "\"]\n"  
+   
+    }
+    listStr = listStr + "} \n"
+
+
+    document.getElementById("textarea_out").value = listStr;
+    copyToClipboard(listStr);
+
+});
+
+$('#btnSwiftyJson').click(e => {
+
+	var data= { "type": "Coding", "height":100 };
+    var v1 = document.getElementById("textarea_in").value;
+	if (v1 != "") {
+        v1 = strToJson(v1);
+        alert("SwiftyJSON success");
+	}   else{
+		v1 =  data;
+	} 
+
+    var json = v1;//document.getElementById("textarea_in").value;
+    var listStr = "";
+    var charType = "String"
+    for( var key in json){
+    	var value = json[key];
+    	// print(key +  "  " + value);
+    	var check = typeof(value);
+    	if (check == "string") {
+    		charType = "String"
+    	}else if(check == "number"){
+    		charType = "Int"
+
+    	} 
+    	listStr = listStr + "var " + key + ":" + charType+"?" + "\n" 
+    }
+    listStr = listStr + "\n"
+    listStr = listStr + "init(_ jsonData:JSON){ \n"
+    for( var key in json){
+    	var value = json[key]; 
+    	var check = typeof(value);
+    	if (check == "string") {
+    		charType = "stringValue"
+    	}else if(check == "number"){
+    		charType = "intValue"
+
+    	} 
+    	listStr = listStr + "    "+key + " = jsonData[\""+ key + "\"]." + charType +" \n"
+   
+    }
+    listStr = listStr + "} \n"
+
+
+    document.getElementById("textarea_out").value = listStr;
+    copyToClipboard(listStr);
+
+});
+
+$('#btnid1').click(e => {
+
+	var data= { "Type": "Coding", "Height":100 };
+    var v1 = document.getElementById("textarea_in").value;
+	if (v1 != "") {
+        v1 = strToJson(v1);
+        alert("oc success");
+	}   else{
+		v1 =  data;
+	} 
+
+    var json = v1;//document.getElementById("textarea_in").value;
+    var listStr = "";
+    var charType = "NSString"
+    var refType = "copy"
+    for( var key in json){
+    	var value = json[key];
+    	// print(key +  "  " + value);
+    	var check = typeof(value);
+    	if (check == "string") {
+    		charType = "NSString * "
+    		refType = "copy"
+    	}else if(check == "number"){
+    		charType = "Int "
+    		refType = "assign"
     	}
     	listStr = listStr + "/* \n"
     	listStr = listStr + "*" + key + ":" + value + "\n"
     	listStr = listStr + "*/" + "\n"
-    	listStr = listStr + "@property(nonatimic,copy)" + "NSString * " + key + "\n";
+    	listStr = listStr + "@property(nonatimic,"+ refType+ ")" + charType + key + ";\n";
     	listStr = listStr + "\n";
     	// listStr = listStr +  check + " " +  key +":"  + value + "\n\n"
     }
@@ -35,7 +168,7 @@ $('#btnid1').click(e => {
 });
 
 
-function strToJson(str){
+function strToJson(str){ 
     return JSON.parse(str);
 }
 
